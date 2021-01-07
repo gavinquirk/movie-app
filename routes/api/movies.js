531ef847ca23ab8d2c543c346dd1aa14d4152123
Gavin Quirk <gavinquirk@gmail.com>
2021-01-06 17:48:04 -0800

    removed scraping from movies controller, use tmbd api instead. Methods for search by id, popular, and similar by id

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -1,70 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
-const scraper = require('../../scrapers/scrapers');
-const { formatImdbId } = require('../../utilities/helpers');
 
-// Load Movie Model
-const Movie = require('../../models/Movie');
+const API_KEY = process.env.TMDB_API_KEY;
+
+// @route   GET api/movies/:id
+// @desc    Get single movie
+// @access  Public
+router.get('/:id', (req, res) => {
+  const movie_id = req.params.id;
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => res.json(json));
+});
 
 // @route   GET api/movies/popular
 // @desc    Retreive most popular movies
 // @access  Public
 router.get('/popular', (req, res) => {
-  scraper.imdbPopular().then((data) => {
-    res.json(data);
-  });
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1&region=wadd`;
 });
 
 // @route   GET api/movies/similar/:id
 // @desc    Retreive movies similar to supplied id
 // @access  Public
-// router.get('/similar/:id', (req, res) => {
-//   const movie_id = req.params.id;
-//   const url = `https://api.themoviedb.org/3/movie/${movie_id}/similar?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`;
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((json) => res.json(json));
-// });
-
-// @route   GET api/movies/top
-// @desc    Retreive top rated movies
-// @access  Public
-router.get('/top', (req, res) => {
-  scraper.imdbTop().then((data) => {
-    res.json(data);
-  });
-});
-
-// @route   GET api/movies/:id
-// @desc    Retreive movie by imdb id
-// @access  Public
-router.get('/:id', (req, res) => {
-  // Check for movie in db BY IMDB ID. If doesnt exist, scrape movie from IMDB and add to db.
-
-  // Retreive submitted imdb id
-  const imdbId = formatImdbId(req.params.id);
-
-  // Search database for movie
-  Movie.findOne({ imdbId }).then((movie) => {
-    // If movie already exists, send db data
-    if (movie) {
-      return res.status(200).json(movie);
-    } else {
-      // If movie does not exist, scrape data from imdb
-      scraper.imdbSingle(imdbId).then((data) => {
-        // Create new Movie object and save
-        const newMovie = new Movie({
-          ...data,
-        });
-        // Save movie to db and respond with data
-        newMovie
-          .save()
-          .then((movie) => res.json({ ...movie._doc }))
-          .catch((error) => console.log(error));
-      });
-    }
-  });
+router.get('/similar/:id', (req, res) => {
+  const movie_id = req.params.id;
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}/similar?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => res.json(json));
 });
 
 module.exports = router;
