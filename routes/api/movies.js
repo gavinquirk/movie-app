319d448a58e870e16ac153750a1a4e752143d8fc
Gavin Quirk <gavinquirk@gmail.com>
2021-01-07 15:24:56 -0800

    get movie reviews, get upcoming movies routes

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -14,17 +14,6 @@ router.get('/top', (req, res) => {
     .then((json) => res.json(json));
 });
 
-// @route   GET api/movies/:id
-// @desc    Get single movie
-// @access  Public
-router.get('/:id', (req, res) => {
-  const movie_id = req.params.id;
-  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => res.json(json));
-});
-
 // @route   GET api/movies/popular
 // @desc    Retreive most popular movies
 // @access  Public
@@ -60,6 +49,39 @@ router.get('/recommendations/:id', (req, res) => {
 router.get('/providers/:id', (req, res) => {
   const movie_id = req.params.id;
   const url = `https://api.themoviedb.org/3/movie/${movie_id}/watch/providers?api_key=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => res.json(json));
+});
+
+// @route   GET api/movies/reviews/:id
+// @desc    Retreive reviews by id
+// @access  Public
+router.get('/reviews/:id', (req, res) => {
+  const movie_id = req.params.id;
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}/reviews?api_key=${API_KEY}&language=en-US&page=1`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => res.json(json));
+});
+
+// @route   GET api/movies/upcoming
+// @desc    Retreive upcoming releases
+// @access  Public
+router.get('/upcoming', (req, res) => {
+  const movie_id = req.params.id;
+  const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => res.json(json));
+});
+
+// @route   GET api/movies/:id
+// @desc    Get single movie
+// @access  Public
+router.get('/:id', (req, res) => {
+  const movie_id = req.params.id;
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
   fetch(url)
     .then((response) => response.json())
     .then((json) => res.json(json));
