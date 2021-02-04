3a4fdc2aa607d4d39cfcabfe20beec69453b3655
Gavin Quirk <gavinquirk@gmail.com>
2021-02-03 16:07:26 -0800

    fetch movie and show data on Home from api

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,24 +1,58 @@
-import React, { Component } from 'react';
-import './Home.css';
-import Card from '../../components/Card/Card';
-import Grid from '../../components/Grid/Grid';
+import React, { useState, useEffect } from 'react';
 import Carousel from '../../components/Carousel/Carousel';
 import logo from './logo-large.png';
+import './Home.css';
 
-export default class Home extends Component {
-  render() {
-    const data = require('./tempdata.json');
-    const movieList = data.results;
-    // TODO: Fetch data from api and store in state
-    return (
-      <div className='Home'>
-        <div className='logo'>
-          <img src={logo} alt={'logo'} />
-          <h1>Movie App</h1>
-        </div>
-        <Carousel category={'Top Rated'} data={movieList} />
-        {/* <Grid data={movieList} /> */}
-      </div>
-    );
-  }
-}
+const Home = (props) => {
+  // State
+  const [topMovies, setTopMovies] = useState([]);
+  const [topShows, setTopShows] = useState([]);
+  const [popularShows, setPopularShows] = useState([]);
+
+  // Hooks
+  useEffect(() => {
+    fetchTopMovies();
+    fetchTopShows();
+    fetchPopularShows();
+  }, []);
+
+  const fetchTopMovies = async () => {
+    const response = await fetch('http://localhost:5000/api/movies/top');
+    const data = await response.json();
+    setTopMovies(data.results);
+  };
+
+  const fetchTopShows = async () => {
+    const response = await fetch('http://localhost:5000/api/shows/top');
+    const data = await response.json();
+    setTopShows(data.results);
+  };
+
+  const fetchPopularShows = async () => {
+    const response = await fetch('http://localhost:5000/api/shows/popular');
+    const data = await response.json();
+    setPopularShows(data.results);
+  };
+
+  console.log(topMovies);
+
+  return (
+    <div className='Home'>
+      <section className='logo'>
+        <img src={logo} alt={'logo'} />
+        <h1>Movie App</h1>
+      </section>
+      <section id='top-rated-movies'>
+        <Carousel category={'Top Rated Movies'} data={topMovies} />
+      </section>
+      <section id='top-rated-shows'>
+        <Carousel category={'Top Rated Shows'} data={topShows} />
+      </section>
+      <section id='popular-shows'>
+        <Carousel category={'Popular Shows'} data={popularShows} />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
