7b3fd86b036465aebdcfa4a3cbd4b3997fe049f0
Gavin Quirk <gavinquirk@gmail.com>
2021-02-03 16:46:49 -0800

    fetchPopularMovies in Movies page, minor style changes

--- client/src/pages/Movies/Movies.js
+++ client/src/pages/Movies/Movies.js
@@ -5,10 +5,14 @@ import './Movies.css';
 const Movies = (props) => {
   // State
   const [topMovies, setTopMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
 
   // Hooks
   useEffect(() => {
     fetchTopMovies();
+    fetchPopularMovies();
+    fetchUpcomingMovies();
   }, []);
 
   const fetchTopMovies = async () => {
@@ -17,13 +21,31 @@ const Movies = (props) => {
     setTopMovies(data.results);
   };
 
+  const fetchPopularMovies = async () => {
+    const response = await fetch('http://localhost:5000/api/movies/popular');
+    const data = await response.json();
+    setPopularMovies(data.results);
+  };
+
+  const fetchUpcomingMovies = async () => {
+    const response = await fetch('http://localhost:5000/api/movies/upcoming');
+    const data = await response.json();
+    setUpcomingMovies(data.results);
+  };
+
   return (
     <div className='Movies'>
       <section>
         <h1>All Movies</h1>
       </section>
-      <section id='top-rated-movies'>
+      <section>
         <Carousel category={'Top Rated Movies'} data={topMovies} />
+      </section>
+      <section>
+        <Carousel category={'Popular Movies'} data={popularMovies} />
+      </section>
+      <section>
+        <Carousel category={'Upcoming Movies'} data={upcomingMovies} />
       </section>
     </div>
   );
