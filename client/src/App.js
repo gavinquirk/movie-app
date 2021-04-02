3487e77fc0460877732c5b8b9e7d7770724bd652
Gavin Quirk <gavinquirk@gmail.com>
2021-04-02 16:46:27 -0700

    basic login page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home/Home';
 import Movies from './pages/Movies/Movies';
 import MovieDetails from './pages/MovieDetails/MovieDetails';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <div className='App'>
         <Navigation />
         <Switch>
+          <Route path='/login' component={Login} />
           <Route path='/movies/:id' component={MovieDetails} />
           <Route exact path='/movies' component={Movies} />
           <Route exact path='/' component={Home} />
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+// import Message from '../components/Message';
+import Loader from '../../components/Loader/Loader';
+import { login } from '../../actions/userActions';
+
+const Login = ({ location, history }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    // If logged in...
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  return (
+    <div className='Login'>
+      {loading && <Loader />}
+      <h1>Sign In</h1>
+      {/* {error && <Message variant='danger'>{error}</Message>} */}
+      <form onSubmit={submitHandler}>
+        {/* Email */}
+        <label>Email Address</label>
+        <input
+          type='email'
+          placeholder='Enter Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {/* Password */}
+        <label>Password</label>
+        <input
+          type='password'
+          placeholder='Enter Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
