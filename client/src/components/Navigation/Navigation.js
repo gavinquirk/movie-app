aefe60a1bdb85692b0e4aea4265c871158f63b7f
Gavin Quirk <gavinquirk@gmail.com>
2021-04-26 13:27:50 -0700

    nav detects if logged in or not, login and logout buttons, styles

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../actions/userActions';
 import './Navigation.css';
 
-// Modules
-import { Link } from 'react-router-dom';
+const Navigation = (props) => {
+  const dispatch = useDispatch();
 
-export default class Navigation extends Component {
-  render() {
-    return (
-      <nav className='Navigation'>
-        <div className='logo'>LOGO</div>
-        <ul className='nav-items'>
-          <li className='nav-item'>
-            <Link to='/'>Home</Link>
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <nav className='Navigation'>
+      <div className='logo'>LOGO</div>
+      <ul className='nav-items'>
+        <li className='nav-item'>
+          <Link to='/'>Home</Link>
+        </li>
+        <li className='nav-item'>
+          <Link to='/movies'>Movies</Link>
+        </li>
+        {/* If userInfo exists, show logged in tools. Else, show logged out tools */}
+        {userInfo ? (
+          <>
+            <li className='nav-item'>
+              <Link to='/profile'>Profile</Link>
+            </li>
+            <li className='nav-item'>
+              <button className='logout-button' onClick={logoutHandler}>
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <button className='login-button'>
+              <Link className='nav-link' to='/login'>
+                Login
+              </Link>
+            </button>
           </li>
-          <li className='nav-item'>
-            <Link to='/movies'>Movies</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
